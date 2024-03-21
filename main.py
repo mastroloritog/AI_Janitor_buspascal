52e45be90541c3ac99d5154568f624372d0e00d8
Gianluca Mastrolorito <gianluca.mastrolorito@studenti.iispascal.it>
2024-03-21 09:12:37 +0100

    Update main.py (parametri classe Robot)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from controller import CentralModule
 from robot_model import Robot
 
 # inserire porta arduino e percorso modello yolov5 corretti
-robot = Robot("COM7", 9600, "C:/Users/Accappi/Desktop/progetti/PJ/prova1/yolov5/runs/train/junkRecognition_model2/weights/best.pt")
+robot = Robot("COMx", 9600, "Path/to/best.pt")
 central_module = CentralModule(robot)
 
 central_module.start()
